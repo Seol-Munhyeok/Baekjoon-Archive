782c27e04c300a6f112f3009d4438ada6ab3a43e
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:57:43 +0900

    [Gold V] Title: 토마토, Time: 2188 ms, Memory: 186204 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,8 +3,7 @@ import sys
 
 m, n = map(int, input().split())
 arr = []
-dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
-queue = deque()
+ones = []
 
 # 입력 처리, 1인 좌표 저장
 for i in range(n):
@@ -12,26 +11,41 @@ for i in range(n):
     for j, val in enumerate(map(int, input().split())):
         row.append(val)
         if val == 1:
-            queue.append((i, j))
+            ones.append((i, j))
     arr.append(row)
 
-# BFS
-while queue:
-    y, x = queue.popleft()
-    for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
-        if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] == 0:
-            arr[ny][nx] = arr[y][x] + 1
-            queue.append((ny, nx))
+# 모든 1에 대해 단계 별로 BFS
+dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
+queue = deque()
+for one in ones:
+    queue.append(one)
 
-# 정답 찾기
-answer = -1e9
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 0:
-            print(-1)
-            sys.exit()
-        if arr[i][j] > answer:
-            answer = arr[i][j]
-            
-print(answer - 1)
+answer = 0
+while queue:
+    # 토마토가 모두 익었는지 체크
+    finished = True
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 0:
+                finished = False
+                break
+        if not finished:
+            break
+    
+    if finished:
+        print(answer)
+        sys.exit()
+
+    # 안 익은 토마토가 있으면 탐색 시작
+    size = len(queue)
+    answer += 1
+    for _ in range(size):
+        y, x = queue.popleft()
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] == 0:
+                arr[ny][nx] = 1
+                queue.append((ny, nx))
+
+# 불가능인 경우
+print(-1)
