ebe622825da83551d97f2cdced92ed64d6823146
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:13:42 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, N, dp[10001];
+int dp[10001][4], T, N;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> T;
-    dp[0] = 1;
-    for (int i = 1; i <= 3; i++){
-        for (int j = 1; j <= 10000; j++){
-            if (j >= i) dp[j] += dp[j - i];
-        }
+    dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
+    for (int i = 4; i < 10001; i++){
+        dp[i][1] = dp[i - 1][1];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+        dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
     }
     while (T--){
         cin >> N;
-        cout << dp[N] << '\n';
+        cout << dp[N][1] + dp[N][2] + dp[N][3] << '\n';
     }
     return 0;
 }
