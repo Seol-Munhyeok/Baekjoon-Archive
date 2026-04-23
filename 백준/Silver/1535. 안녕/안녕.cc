a030a2841b33d191f1ebf1e3ad84174bc00d103b
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:11:00 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, L[2002], J[2002];
+int N, dp[21][101], L[2002], J[2002];
+//dp[i][w] = 최대 체력이 w인 경우 i번째까지 판단했을 때의 최대 기쁨
 
 int knapsack(int i, int W){
     if (i <= 0 || W <= 0) return 0;
+    int &ret = dp[i][W];
+    if (ret != -1) return ret;
     if (L[i] >= W) return knapsack(i - 1, W);
     else {
         int not_included = knapsack(i - 1, W);
         int included = knapsack(i - 1, W - L[i]) + J[i];
-        return max(not_included, included);
+        return ret = max(not_included, included);
     }
 }
 
@@ -18,6 +21,7 @@ int main(){
     cin >> N;
     for (int i = 1; i <= N; i++) cin >> L[i];
     for (int i = 1; i <= N; i++) cin >> J[i];
+    memset(dp, -1, sizeof(dp));
     cout << knapsack(N, 100) << '\n';
     return 0;
 }
