c90cc4e5f9ff4de3a54f2b9479b02e5ca3335cbc
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:19:07 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 4 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt, ret;
-string str;
-stack<int> s;
-
+int n, d[200001], ret, cnt;
+string s;
+stack<int> stk;
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n;
-    cin >> str;
-    s.push(-1);
+    cin >> n >> s;
     for (int i = 0; i < n; i++){
-        if (str[i] == '(') s.push(i);
-        if (str[i] == ')'){
-            s.pop();
-            if (s.size()) {
-                ret = max(ret, i - s.top());
-            }
-            else s.push(i);
+        if (s[i] == '(') stk.push(i);
+        // 바른 괄호문자열이면 d에 1을 할당한다.
+        else if (stk.size()){
+            d[i] = d[stk.top()] = 1;
+            stk.pop();
         }
+    }
+    // 가장 긴 1의 길이를 구한다.
+    for (int i = 0; i < n; i++){
+        if (d[i]){
+            cnt++;
+            ret = max(ret, cnt);
+        }
+        else cnt = 0;
     }
     cout << ret << '\n';
     return 0;
