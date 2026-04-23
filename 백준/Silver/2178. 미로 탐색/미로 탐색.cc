d32abea941b05a9cecc506c0c1ec683eaf3abef4
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:27:12 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2240 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[104][104], visited[104][104];
+int N, M, y, x, ny, nx;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+string str;
+queue<pair<int, int> > q;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        cin >> str;
+        for (int j = 0; j < M; j++){
+            a[i][j] = str[j] - '0';
+        }
+    }
+
+    visited[0][0] = 1;
+    q.push({0, 0});
+    while (q.size()){
+        tie(y, x) = q.front();
+        q.pop();
+        for (int dir = 0; dir < 4; dir++){
+            ny = y + dy[dir];
+            nx = x + dx[dir];
+            if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+            if (a[ny][nx] == 0 || visited[ny][nx]) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+
+    cout << visited[N - 1][M - 1] << '\n';
+
+    return 0;
+}
