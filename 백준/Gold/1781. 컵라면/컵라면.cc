b8335543a76a828ae96a0143a8e7610080552efc
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:18:02 +0900

    [Gold II] Title: 컵라면, Time: 68 ms, Memory: 6244 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, d, c, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cin >> d >> c;
+        v.push_back({d, c});
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < N; i++){
+        pq.push(v[i].second);
+        if (pq.size() > v[i].first) pq.pop();
+    }
+    while (pq.size()){
+        ret += pq.top();
+        pq.pop();
+    }
+    cout << ret << '\n';
+    return 0;
+}
