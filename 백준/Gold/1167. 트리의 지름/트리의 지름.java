c3c40ba961e2e933242ded4d9aa5588edf7487a3
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:48:22 +0900

    [Gold II] Title: 트리의 지름, Time: 784 ms, Memory: 121624 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	
+	static String next() throws Exception {
+		while (st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine());
+		}
+		return st.nextToken();
+	}
+	
+	static int nextInt() throws Exception {
+		return Integer.parseInt(next());
+	}
+	
+	static int N, farNode;
+	static long maxDist;
+	static List<int[]>[] adj; // { v, w }
+	static boolean[] visited;
+	
+	static void dfs(int cur, int parent, long dist) {
+		if (dist > maxDist) {
+			farNode = cur;
+			maxDist = dist;
+		}
+		for (int[] c : adj[cur]) {
+			int next = c[0], w = c[1];
+			if (next == parent) continue;
+			
+			dfs(next, cur, dist + w);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = nextInt();
+		adj = new List[N + 1];
+		for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
+		visited = new boolean[N + 1];
+		
+		for (int i = 0; i < N; i++) {
+			int u = nextInt();
+			while (true) {
+				int v = nextInt();
+				if (v == -1) break;
+				int w = nextInt();
+				
+				adj[u].add(new int[] {v, w});
+			}
+		}
+		
+		farNode = -1;
+		maxDist = -1;
+		dfs(1, -1, 0);
+		int end1 = farNode;
+		
+		farNode = -1;
+		maxDist = -1;
+		dfs(end1, -1, 0);
+		
+		System.out.println(maxDist);
+	}
+}
