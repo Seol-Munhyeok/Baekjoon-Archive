42a0dece2657bf6439659096c842372658462804
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:19:55 +0900

    [Silver V] Title: 피카츄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14405. 피카츄/피카츄.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s, str;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> str;
+    s = "";
+    int len = 0;
+    bool ok = 1;
+    for (char ch : str){
+        s += ch;
+        if (s == "pi" || s == "ka" || s == "chu"){
+            len = 0;
+            s = "";
+        }
+        else {
+            len++;
+            if (len >= 3) { ok = 0; break; }
+        }
+    }
+    if (len > 0) ok = 0;
+    cout << (ok ? "YES" : "NO") << '\n';
+    return 0;
+}
