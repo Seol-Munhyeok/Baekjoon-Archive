085e1506bc25a4ca7b782530efb7301088f6b239
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:25:24 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const char open[] = {'(', '['};
+const char close[] = {')', ']'};
+string s;
+
+bool check(string s){
+    stack<char> stk;
+    for (char ch : s){
+        for (int i : {0, 1}){
+            if (ch == open[i]) stk.push(ch);
+            else if (ch == close[i]){
+                if (stk.size()  && stk.top() == open[i])
+                    stk.pop();
+                else return false;
+            }
+        }
+    }
+    return stk.empty();
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    while (true){
+        getline(cin, s); if (s == ".") break;
+        if (check(s)) cout << "yes\n";
+        else cout << "no\n";
+    }
+    return 0;
+}
