6ccf31a889d5e3daea11ff43ad9b26e17d52693c
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:27:00 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[101][101], visited[101][101];
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1}; 
+int N, ny, nx, cnt, ret, H;
+
+void dfs(int y, int x){
+    visited[y][x] = 1;
+    for (int dir = 0; dir < 4; dir++){
+        ny = y + dy[dir];
+        nx = x + dx[dir];
+        if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+        if (a[ny][nx] > H && !visited[ny][nx]) 
+            dfs(ny, nx);
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            cin >> a[i][j];
+        }   
+    }
+    while (H <= 100){
+        memset(visited, 0, sizeof(visited)); 
+        cnt = 0;
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                if (a[i][j] > H && !visited[i][j]) {
+                    dfs(i, j); cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+        H++;
+    }
+    cout << ret << '\n';
+    return 0;
+}
