112fc3096cbf4f6e147f562b520a35242071bff3
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:11:15 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -4,16 +4,15 @@ using namespace std;
 int N, dp[21][101], L[2002], J[2002];
 //dp[i][w] = 최대 체력이 w인 경우 i번째까지 판단했을 때의 최대 기쁨
 
-int knapsack(int i, int W){
+int knapsack(int i, int W, int w[], int p[]){
     if (i <= 0) return 0;
     if (W <= 0) return -1e9;
     int &ret = dp[i][W];
     if (ret != -1) return ret;
-    if (L[i] > W) return knapsack(i - 1, W);
+    if (w[i] > W) return knapsack(i - 1, W, w, p);
     else {
-        int not_included = knapsack(i - 1, W);
-        int included = 0;
-        if (L[i] < W) included = knapsack(i - 1, W - L[i]) + J[i];
+        int not_included = knapsack(i - 1, W, w, p);
+        int included = (w[i] < W) ? (knapsack(i - 1, W - w[i], w, p) + p[i]) : 0;
         return ret = max(not_included, included);
     }
 }
@@ -24,6 +23,6 @@ int main(){
     for (int i = 1; i <= N; i++) cin >> L[i];
     for (int i = 1; i <= N; i++) cin >> J[i];
     memset(dp, -1, sizeof(dp));
-    cout << knapsack(N, 100) << '\n';
+    cout << knapsack(N, 100, L, J) << '\n';
     return 0;
 }
