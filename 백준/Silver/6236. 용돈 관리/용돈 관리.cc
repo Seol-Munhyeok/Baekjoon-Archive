9c1c3c1d7fe1a4f5a052c68d96d8486385bcbb9c
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:15:14 +0900

    [Silver I] Title: 용돈 관리, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll N, M, mx, sum, lo, hi, ret, a[100004];
+
+bool check(ll mid){
+    //if (mx > mid) return false;
+    ll cnt = 1, tmp = mid;
+    for (int i = 0; i < N; i++){
+        if (mid - a[i] < 0){
+            cnt++;
+            mid = tmp;
+        }
+        mid -= a[i];
+    }
+    return cnt <= M;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        cin >> a[i];
+        mx = max(mx, a[i]);
+        sum += a[i];
+    }
+    lo = mx; hi = sum;
+    while (lo <= hi){
+        ll mid = (lo + hi) / 2;
+        if (check(mid)){
+            hi = mid - 1;
+            ret = mid;
+        }
+        else lo = mid + 1;
+    }
+    cout << ret << '\n';
+    return 0;
+}
