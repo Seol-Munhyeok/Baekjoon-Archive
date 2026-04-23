a1036a11b00f8eee9b2cf0db8c95fa9dea787aa4
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:24:59 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,33 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, r, temp, root;
-vector<int> adj[54];
+int parent[50], visited[50];
+vector<int> adj[50];
+int N, M, cnt, root;
 
-int dfs(int here){
-    int ret = 0;
-    int child = 0;
+void dfs(int here){
+    visited[here] = 1;
+    if (adj[here].empty()) cnt++;
     for (int there : adj[here]){
-        if (there == r) continue; // 지울 노드면 탐색하지 않음
-        ret += dfs(there);
-        child++;
+        if (!visited[there]) dfs(there);
     }
-    if (child == 0) return 1;
-    return ret;
+    return;
 }
 
 int main(){
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        cin >> temp;
-        if (temp == -1) root = i;
-        else adj[temp].push_back(i);
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cin >> parent[i];
     }
-    cin >> r;
-    if (r == root){
-        cout << 0 << '\n'; 
-        return 0;
-    }
-    cout << dfs(root) << '\n';
+    for (int i = 0; i < N; i++){
+        if (parent[i] == -1) root = i;
+        else adj[parent[i]].push_back(i);
+    }   
+    cin >> M;
+    
+    if (root == M) cnt = 0;
+    else if (N == 2) cnt = 1;
+    else {
+        auto t = find(adj[parent[M]].begin(), adj[parent[M]].end(), M);
+        adj[parent[M]].erase(t);
+        dfs(root);
+    } 
+    cout << cnt << '\n';
     return 0;
 }
