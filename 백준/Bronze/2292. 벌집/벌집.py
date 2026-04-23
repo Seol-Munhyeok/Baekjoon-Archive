dc30fff8c99dcf92540a6b5fb1ff25eda136406a
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:39:04 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,32 +1,23 @@
-import math
+def get_max_address(n):
+    """ 방의 번호 n이 주어질 때, 주소의 최댓값을 구하는 함수
+     f(1) = 1
+     f(2) = 7
+     f(3) = 19
+     f(4) = 37 ...
+     f(n) = f(n-1) + 6(n-1)의 점화 관계가 나옴
+     이는 공차가 6(n-1)인 등차수열과 같고 이를 통해 일반항을 구하면
+     f(n) = f(1) + 6(1+2+3+...+(n-1))
+     이고 등차수열의 합 공식을 적용하면 일반항이 나온다. """
+
+    return 1 + 6 * n * (n-1) / 2
 
 
-def count_room(address):
-    """
-    방의 주소 address가 주어질 때 최소의 방의 개수 n을 구하는 함수이다.
+room_address = int(input())
+cnt = 1
+while True:
+    if room_address <= get_max_address(cnt):
+        break
+    else:
+        cnt += 1
 
-    이를 구하기 위해 먼저
-    방의 개수 n이 주어질 때 그 방의 최대 주소값을 구하는 일반항
-    f(n) = 3n^2 - 3n + 1
-     을 앞서 구하였다.
-
-     이를 이용하여 f(n) = 3n^2 - 3n + 1 >= address를 만족하는
-     n의 최솟값을 구하면 된다.
-
-     n^2 - n >= (address - 1) / 3
-     이고 n>=0 이므로
-
-     n^2 >= n^2 - n >= (address - 1) / 3
-     n >= sqrt((address - 1) / 3)
-
-     여기서 n은 정수이므로 반올림한다.
-     또 처음 벌집도 세어줘야하므로 1을 더한다.
-    """
-
-    return round(math.sqrt((address - 1) / 3)) + 1
-
-
-address = int(input())
-print(count_room(address))
-
-# 질문 게시판의 내용을 참고하였음
+print(cnt)
