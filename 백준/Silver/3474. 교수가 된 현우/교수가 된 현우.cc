4a7a3666574fc70c95990d3084319c1f3ea9a6fc
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:26:07 +0900

    [Silver III] Title: 교수가 된 현우, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -4,15 +4,18 @@ using namespace std;
 int T, N;
 
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> T;
     while (T--){
         cin >> N;
-        int ret = 0;
-        for (int i = 5; i <= N; i *= 5){
-            ret += N / i;
+        int ret2 = 0, ret5 = 0;
+        for (int i = 2; i <= N; i *= 2){
+            ret2 += N / i;
         }
-        cout << ret << '\n';
+        for (int i = 5; i <= N; i *= 5){
+            ret5 += N / i;
+        }
+        cout << min(ret2, ret5) << '\n';
     }
     return 0;
 }
