0e185e4b5ece238aa2fa8b41874b810aee2479a8
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:47:40 +0900

    [Silver V] Title: 다리 놓기, Time: 96 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,29 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int T = sc.nextInt();
-		
-		// 조합 테이블 채우기
-		int[][] combi = new int[31][31];
-		for (int i = 1; i < 30; i++) {
-			combi[i][1] = i;
-			combi[i][i] = 1;
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static String next() throws Exception {
+		while (st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine());
 		}
+		return st.nextToken();
+	}
+	
+	static int nextInt() throws Exception {
+		return Integer.parseInt(next());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		
-		for (int i = 2; i < 30; i++) {
-			for (int j = 2; j < i; j++) {
-				combi[i][j] = combi[i - 1][j - 1] + combi[i - 1][j];
-			}
-		}
-		
+		int T = nextInt();
 		for (int tc = 1; tc <= T; tc++) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			sb.append(combi[m][n]).append("\n");
+			int N = nextInt(), M = nextInt();
+			long dp[][] = new long[31][31];
+			
+			for (int n = 0; n <= 30; n++) {
+				dp[n][0] = 1;
+				dp[n][n] = 1;
+			}
+			
+			for (int n = 1; n <= 30; n++) {
+				for (int r = 1; r < n; r++) {
+					dp[n][r] = dp[n - 1][r - 1] + dp[n - 1][r];
+				}
+			}
+			
+			sb.append(dp[M][N]).append("\n");
 		}
 		
 		System.out.println(sb);
