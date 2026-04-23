86c966a07674278b4d9064b2a19613e081a74e7b
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:54:03 +0900

    [Bronze I] Title: A/B - 2, Time: 92 ms, Memory: 12908 KB, Score: 3 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.java
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -5,19 +5,29 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		int a = sc.nextInt(); int b = sc.nextInt();
+		boolean[] visited = new boolean[10];
 		
 		// 정수 부분
 		sb.append(a / b).append(".");
-		
 		// 소수 부분
+		StringBuilder frac = new StringBuilder();
 		int rem = a % b;
-		for (int i = 0; i < 2000; i++) {
-			rem *= 10;
-			int digit = rem / b;
-			sb.append(digit);
-			rem %= b;
-			if (rem == 0) break;
+		rem *= 10;
+		int q = rem / b; int r = rem % b;
+		if (r == 0) {
+			sb.append(q);
+			System.out.println(sb);
+			return;
 		}
+		while (r != 0 && !visited[q]) {
+			frac.append(q);
+			visited[q] = true;
+			r *= 10;
+			q = r / b; r = r % b;
+		}
+		
+		for (int i = 0; i < 2000 / frac.length(); i++) 
+			sb.append(frac);
 		System.out.println(sb);
 	}
 }
