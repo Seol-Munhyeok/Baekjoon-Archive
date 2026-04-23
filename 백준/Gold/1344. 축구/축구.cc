759a1afdbbd578a3cc6e3447ae82897cb9f57934
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:09:39 +0900

    [Gold V] Title: 축구, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/1344. 축구/축구.cc
+++ 백준/Gold/1344. 축구/축구.cc
@@ -2,8 +2,9 @@
 using namespace std;
 
 int x, y;
-double p, q, ret;
+double p, q, ret, prob;
 int notPrimes[] = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18};
+int combis[] = {1, 18, 3060, 18564, 43758, 48620, 18564, 3060, 816, 153, 1};
 double dp[20][20][20];
 
 int main(){
@@ -11,13 +12,13 @@ int main(){
     cin >> x >> y;
     p = x / 100.0;
     q = y / 100.0;
-    dp[1][0][0] = (1 - p) * (1 - q);
-    dp[1][0][1] = (1 - p) * q;
-    dp[1][1][0] = p * (1 - q);
-    dp[1][1][1] = p * q;
+    dp[1][0][0] = (1.0 - p) * (1.0 - q);
+    dp[1][0][1] = (1.0 - p) * q;
+    dp[1][1][0] = p * (1.0 - q);
+    dp[1][1][1] = p * q; 
     for (int i = 2; i <= 18; i++){
-        for(int a = 0; a <= 18; a++){
-            for(int b = 0; b <= 18; b++){
+        for(int a = 0; a <= 18; ++a) {
+            for(int b = 0; b <= 18; ++b) {
                 dp[i][a][b] += dp[i - 1][a - 1][b - 1] * p * q;
                 dp[i][a][b] += dp[i - 1][a][b - 1] * (1 - p) * q;
                 dp[i][a][b] += dp[i - 1][a - 1][b] * p * (1 - q);
