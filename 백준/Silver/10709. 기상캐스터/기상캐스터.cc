1db06b2ad617afc5ca477a56bd2fbb32a6a477a2
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:26:13 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a[103][103];
+int b[103][103];
+int H, W, ret;
+bool flag;
+
+int main(){
+    cin >> H >> W;
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < H; i++){
+        flag = 0; ret = 1;
+        for (int j = 0; j < W; j++){
+            if (a[i][j] == '.'){
+                if (flag == 0) b[i][j] = -1;
+                if (flag == 1) b[i][j] = ret++;
+            }
+            if (a[i][j] == 'c') {
+                flag = 1; b[i][j] = 0; ret = 1;
+            }
+        }
+    }
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
+            cout << b[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
