bd93cf77aeeaaec78316de3c5b636f1ab473c9cd
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:24:21 +0900

    [Gold V] Title: 치킨 배달, Time: 12 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,49 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, a[54][54], result = 1e9;
-vector<vector<int>> chickenList;
-vector<pair<int, int>> _home, chicken;
+int N, M, tmp;
+vector<pair<int, int>> ck, ho, v;  // ck : chicken, ho : house
+vector<int> ret;
 
-void combi(int start, vector<int> v){
-    if (v.size() == M){
-        chickenList.push_back(v);
+int distance(pair<int, int> x, pair<int, int> y){
+    return abs(x.first - y.first) + abs(x.second - y.second);
+}
+
+int go(vector<pair<int, int>> v){
+    // 주어진 치킨 집 좌표를 인자로 받아 최소 치킨 거리 출력
+    int ret = 0;
+    for (int i = 0; i < ho.size(); i++){
+        int mn = 1e9;
+        for (int j = 0; j < v.size(); j++){
+            mn = min(mn, distance(ho[i], v[j]));
+        }
+        ret += mn;
+    }
+    return ret;
+}
+
+void combi(int start, int k){
+    if (v.size() == k) {
+        ret.push_back(go(v));
         return;
     }
-    for (int i = start + 1; i < chicken.size(); i++){
-        v.push_back(i);
-        combi(i, v);
+    for (int i = start + 1; i < ck.size(); i++){
+        v.push_back(ck[i]);
+        combi(i, k);
         v.pop_back();
     }
     return;
 }
 
-
 int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> M;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
-            cin >> a[i][j];
-            if (a[i][j] == 1) _home.push_back({i, j});
-            if (a[i][j] == 2) chicken.push_back({i, j});
+            cin >> tmp;
+            if (tmp == 1) ho.push_back({i, j});
+            if (tmp == 2) ck.push_back({i, j});
         }
+    }
+    
+    // ck 벡터에서 최대 M개 까지 뽑는 벡터 생성
+    for (int i = 1; i <= M; i++){
+        v.clear();
+        combi(-1, i);
     }
 
-    vector<int> v;
-    combi(-1, v);
+    cout << *min_element(ret.begin(), ret.end());
     
-    for (vector<int> cList : chickenList){
-        int ret = 0;
-        for (pair<int, int> home : _home){
-            int _min = 1e9;
-            for (int ch : cList) {
-                int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
-                _min = min(_min, _dist);
-            }
-            ret += _min;
-        }
-        result = min(result, ret);
-    }
-    cout << result << '\n';
     return 0;
 }
