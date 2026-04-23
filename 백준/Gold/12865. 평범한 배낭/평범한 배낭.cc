0916a9cffc9709084e7b7cb583efa452e073438f
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:11:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, w, v, ret, dp[103][100003];
-vector<int> W, V;
-// dp[i][w] : 배낭 용량이 w일 때, 아이템 1, 2, ... i로 얻을 수 있는 최대 이득
+int N, K, w, v, ret, dp[100001];
+// dp[i] = 무게 i로 만들 수 있는 최대 가치를 저장
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> N >> K;  // N : 물품 개수, K 최대 용량
-    W.push_back(0);  // 0번째 인덱스 채우기
-    V.push_back(0);  // 0번째 인덱스 채우기
-    for (int i = 1; i <= N; i++){
+    cin >> N >> K;
+    while (N--){
         cin >> w >> v;
-        W.push_back(w);
-        V.push_back(v);
-    }
-    for (int i = 1; i <= N; i++){
-        for (int weight = 1; weight <= K; weight++){
-            if (W[i] > weight) dp[i][weight] = dp[i - 1][weight];
-            else dp[i][weight] = max(dp[i - 1][weight], V[i] +  dp[i - 1][weight - W[i]]); 
+        for (int weight = K; weight >= w; weight--){
+            dp[weight] = max(dp[weight], dp[weight - w] + v);
         }
     }
-    cout << dp[N][K] << "\n";
+    cout << dp[K] << "\n";
     return 0;
 }
