325c0e480ce6d8e6f45e9466a74906d695c575d0
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:24:26 +0900

    [Gold V] Title: 치킨 배달, Time: 8 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, tmp;
+int a[53][53], N, M, tmp;
 vector<pair<int, int>> ck, ho, v;  // ck : chicken, ho : house
 vector<int> ret;
 
@@ -35,14 +35,15 @@ void combi(int start, int k){
     return;
 }
 
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> M;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
-            cin >> tmp;
-            if (tmp == 1) ho.push_back({i, j});
-            if (tmp == 2) ck.push_back({i, j});
+            cin >> a[i][j];
+            if (a[i][j] == 1) ho.push_back({i, j});
+            if (a[i][j] == 2) ck.push_back({i, j});
         }
     }
     
