c0ad54175f7b5415377600131182a2ec3081b8bc
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:30:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 168 ms, Memory: 115376 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+import sys
+
+open_brackets = ['(', '[']
+close_brackets = [')', ']']
+
+
+def is_balanced(sentence):
+    stack = []
+    for letter in sentence:
+        if letter in open_brackets:
+            stack.append(open_brackets.index(letter))
+        if letter in close_brackets:
+            if len(stack) == 0:
+                return 'no'
+            else:
+                if stack[-1] == close_brackets.index(letter):
+                    stack.pop()
+                else:
+                    return 'no'
+    if len(stack) == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+while True:
+    sentence = sys.stdin.readline().rstrip()
+    if sentence == '.':
+        break
+    print(is_balanced(sentence))
