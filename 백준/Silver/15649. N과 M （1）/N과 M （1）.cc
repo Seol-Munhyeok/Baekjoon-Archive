e7118578ca63d7e4919f3fa8fd881b50af49db91
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:08:53 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int a[10];
+bool used[10];
+
+void go(int k){
+    if (k == M){
+        for (int i = 0; i < M; i++){
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= N; i++){
+        if (!used[i]){
+            a[k] = i;
+            used[i] = true;
+            go(k + 1);
+            used[i] = false;
+        }
+    }
+    return;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    go(0);
+    return 0;
+}
