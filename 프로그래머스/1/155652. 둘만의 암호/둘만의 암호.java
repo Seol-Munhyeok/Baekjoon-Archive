3bc8bb5c041e3797a802334cc51d74d88994df05
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-08-25 23:59:46 +0900

    [level 1] Title: 둘만의 암호, Time: 0.24 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,20 +1,26 @@
+import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        for (char ch : s.toCharArray()) {
-            answer += jump(ch, skip, index);
+        StringBuilder answer = new StringBuilder();
+        
+        Set<Character> skipSet = new HashSet<>();
+        for (char ch : skip.toCharArray()) {
+            skipSet.add(ch);
         }
-        return answer;
+        
+        for (char ch : s.toCharArray()) {
+            answer.append(jump(ch, skipSet, index));
+        }
+        return answer.toString();
     }
     
-    private char jump(char ch, String skip, int index) {
+    private char jump(char ch, Set<Character> skipSet, int index) {
         int jumpCount = 0;
         while (jumpCount < index) {
-            char nextChar = (char)((ch - 'a' + 1) % 26 + 'a');
-            if (skip.indexOf(nextChar) == -1) {
+            ch = (char)((ch - 'a' + 1) % 26 + 'a');
+            if (!skipSet.contains(ch)) {
                 jumpCount++;
             }    
-            ch = nextChar;
         }
         return ch;
     }
